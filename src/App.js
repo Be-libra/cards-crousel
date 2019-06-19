61944467d25d434d8390668f72e178164b24e582
Be-libra <50490129+Be-libra@users.noreply.github.com>
2019-06-19 16:51:37 +0530

    uploading react cards files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Deck from "./components/Deck";
+import './styles/Deck.css';
+
+const App = () =>{
+	return(
+	<div className='view'>
+	<Deck />
+	</div>
+	);}
+
+export default App;
